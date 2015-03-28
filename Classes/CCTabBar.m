4b98f302abce2529bcc2b08a164e67dbd525b578
Can <chencanjob@gmail.com>
2015-03-29 01:58:19 +0800

    Minor change

--- Classes/CCTabBar.m
+++ Classes/CCTabBar.m
@@ -25,6 +25,9 @@
         self.userInteractionEnabled = YES;
         self.selectedIndex = NSUIntegerMax;
         self.bottomViewHeight = 1;
+        self.tabButtonArray = [[NSMutableArray alloc] initWithCapacity:2];
+        self.bottomView = [[UIView alloc] initWithFrame:CGRectZero];
+        [self addSubview:self.bottomView];
     }
     return self;
 }
@@ -33,20 +36,20 @@
 #pragma mark Private
 
 -(void)tabTouched:(UIButton *)sender {
-	if (self.delegate) {
+    if (self.delegate) {
         self.selectedIndex = sender.tag - 100;
-		[self.delegate tabBar:self didSelectTabAtIndex:self.selectedIndex];
+        [self.delegate tabBar:self didSelectTabAtIndex:self.selectedIndex];
         [self setNeedsLayout];
-	}
+    }
 }
 
 - (void)autoTouchTab:(int)index
 {
     if (self.delegate) {
         [self setSelectedIndex:index animated:NO];
-		[self.delegate tabBar:self didSelectTabAtIndex:self.selectedIndex];
+        [self.delegate tabBar:self didSelectTabAtIndex:self.selectedIndex];
         [self setNeedsLayout];
-	}
+    }
 }
 
 #pragma mark -
@@ -54,15 +57,15 @@
 
 
 - (void)layoutSubviews {
-	[super layoutSubviews];
+    [super layoutSubviews];
     
-	
-	CGRect tabFrame = CGRectMake(self.tabOrigin, 0, self.tabWidth, self.bounds.size.height - 4);
-	
+    
+    CGRect tabFrame = CGRectMake(self.tabOrigin, 0, self.tabWidth, self.bounds.size.height - 4);
+    
     int i = 0;
-	for (UIButton *button in self.tabButtonArray) {
-		button.frame = tabFrame;
-		tabFrame.origin.x += self.tabDistance + self.tabWidth;
+    for (UIButton *button in self.tabButtonArray) {
+        button.frame = tabFrame;
+        tabFrame.origin.x += self.tabDistance + self.tabWidth;
         
         button.titleLabel.alpha = 0.5;
         
@@ -88,53 +91,45 @@
                 
                 self.animated = YES;
             }
-
+            
         }
         
         i++;
-	}
+    }
     
-
+    
 }
 
 #pragma mark -
 #pragma mark Public
 
 - (void)setTabNum:(int)aTabNum {
-    if (!self.tabButtonArray) {
-		self.tabButtonArray = [[NSMutableArray alloc] initWithCapacity:2];
-	}
-	
-    if (!self.bottomView) {
-        self.bottomView = [[UIView alloc] initWithFrame:CGRectZero];
-        [self addSubview:self.bottomView];
-    }
     
-	if ([self.tabButtonArray count] != aTabNum) {
-		if (aTabNum < [self.tabButtonArray count]) {
-			while ([self.tabButtonArray count] > aTabNum) {
-				[[self.tabButtonArray lastObject] removeFromSuperview];
-				[self.tabButtonArray removeLastObject];
-			}
-		} else {
-			UIButton *button = nil;
-			for (NSUInteger i = [self.tabButtonArray count]; i < aTabNum; i++) {
-				button = [UIButton buttonWithType:UIButtonTypeCustom];
-				button.backgroundColor = [UIColor clearColor];
+    if ([self.tabButtonArray count] != aTabNum) {
+        if (aTabNum < [self.tabButtonArray count]) {
+            while ([self.tabButtonArray count] > aTabNum) {
+                [[self.tabButtonArray lastObject] removeFromSuperview];
+                [self.tabButtonArray removeLastObject];
+            }
+        } else {
+            UIButton *button = nil;
+            for (NSUInteger i = [self.tabButtonArray count]; i < aTabNum; i++) {
+                button = [UIButton buttonWithType:UIButtonTypeCustom];
+                button.backgroundColor = [UIColor clearColor];
                 button.titleLabel.font = [UIFont systemFontOfSize:14];
                 [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
                 button.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
                 button.reversesTitleShadowWhenHighlighted = YES;
-				button.tag = 100 + i;
-				[button addTarget:self 
-						   action:@selector(tabTouched:) 
-				 forControlEvents:UIControlEventTouchDown];
-				[self.tabButtonArray addObject:button];
-				
-				[self addSubview:button];
-			}
-		}
-	}
+                button.tag = 100 + i;
+                [button addTarget:self
+                           action:@selector(tabTouched:)
+                 forControlEvents:UIControlEventTouchDown];
+                [self.tabButtonArray addObject:button];
+                
+                [self addSubview:button];
+            }
+        }
+    }
     
     self.bottomView.hidden = (0 == aTabNum);
 }
@@ -150,7 +145,7 @@
     _tabDistance = aTabDistance;
     [self setNeedsLayout];
 }
-    
+
 
 - (void)setSelectedIndex:(int)aSelectedIndex animated:(BOOL)anAniamted {
     if (aSelectedIndex != self.selectedIndex) {
